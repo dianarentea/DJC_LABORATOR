48987ed25016d791c1decbc3bd6137ff79b08b22
dianarentea <rentea88@gmail.com>
2024-05-16 17:32:49 +0300

    added 'dame'

--- Laborator1/Assets/Scripts/CheckersBoard.cs
+++ Laborator1/Assets/Scripts/CheckersBoard.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class CheckersBoard : MonoBehaviour
 {
-    public GameObject blackPiecePrefab; // Referința la prefab-ul piesei negre
-    public GameObject whitePiecePrefab; // Referința la prefab-ul piesei albe
-    public Transform piecesParent; // Parent pentru piese pentru a menține ierarhia organizată
-    public float squareSize = 1.0f; // Presupunem că fiecare pătrat este de 1 unitate
+    public GameObject blackPiecePrefab; 
+    public GameObject whitePiecePrefab; 
+    public float squareSize = 1.0f;
 
-    // Poziția inițială a colțului stâng jos al tablei de șah
     public Vector3 boardStart = new Vector3(-3.5f, 0.0f, -3.5f);
 
     void Start()
@@ -19,17 +17,15 @@ public class CheckersBoard : MonoBehaviour
 
     void PlacePieces()
     {
-        // Offset pentru a centra piesele pe pătratele negre
         Vector3 pieceOffset = new Vector3(0.5f, 0, 0.5f);
 
-        for (int y = 0; y < 3; y++) // Primele 3 rânduri pentru piesele negre
+        for (int y = 0; y < 3; y++) 
         {
             for (int x = 0; x < 8; x++)
             {
-                // Calcul poziției centrate pe căsuța de șah
+              
                 Vector3 blackPosition = boardStart + new Vector3(x, 0, y) * squareSize + pieceOffset;
 
-                // Plasează piesele negre doar pe pătratele negre
                 if ((x + y) % 2 != 0)
                 {
                     PlacePiece(blackPosition, blackPiecePrefab);
@@ -37,14 +33,12 @@ public class CheckersBoard : MonoBehaviour
             }
         }
 
-        for (int y = 5; y < 8; y++) // Ultimele 3 rânduri pentru piesele albe
+        for (int y = 5; y < 8; y++) 
         {
             for (int x = 0; x < 8; x++)
             {
-                // Calcul poziției centrate pe căsuța de șah
                 Vector3 whitePosition = boardStart + new Vector3(x, 0, y) * squareSize + pieceOffset;
 
-                // Plasează piesele albe doar pe pătratele negre
                 if ((x + y) % 2 != 0)
                 {
                     PlacePiece(whitePosition, whitePiecePrefab);
@@ -55,11 +49,8 @@ public class CheckersBoard : MonoBehaviour
 
     void PlacePiece(Vector3 position, GameObject piecePrefab)
     {
-        // Creează o piesă și ajustează-i poziția
         GameObject newPiece = Instantiate(piecePrefab, position, Quaternion.identity);
-        newPiece.transform.SetParent(piecesParent);
 
-        // Ajustează poziția pe axa Y dacă este necesar
         newPiece.transform.localPosition += new Vector3(0, 0, 0);
     }
 }
